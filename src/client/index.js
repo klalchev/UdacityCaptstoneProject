d4fe62b6d01fc855c995305209ca8fbd59ff0b9b
Kostadin <kostadin.lalchev88@gmail.com>
2020-09-15 07:28:27 +0300

    feat:Create ToDoList Functions

--- src/client/index.js
+++ src/client/index.js
@@ -1,6 +1,10 @@
 import {performAction} from './js/app'
 import {func} from './js/countdown'
 import {distance} from './js/countdown'
+import {newElement} from './js/ToDoList'
+import {addCloseButton} from './js/ToDoList'
+import {removeList} from './js/ToDoList'
+import {checkItem} from './js/ToDoList'
 
 document.addEventListener('DOMContentLoaded', (event) => {
     // install listeners here
@@ -8,12 +12,29 @@ document.addEventListener('DOMContentLoaded', (event) => {
         performAction();
         func();
 });
+    document.querySelector('.addBtn').addEventListener('click', () => {
+    newElement();
+    addCloseButton();
 });
+
+    document.querySelector('.close').addEventListener('click', () => {
+        removeList();
+    }); //event listener for checked and removed
+
+    document.getElementById('myUL').addEventListener('click', () => {
+        checkItem();
+    });
+
+}); // Event listeners do not affect DOM Loading. It is the other way around. If your JS kicks into action before DOM Loading is done, it can mess up the functions. For instance, imagine trying to install event listener on a button when it is not yet available in DOM. It simply won't work. It is hence advisable to install event listeners only once DOM is do
 
 import './styles/style.scss'
 
 export {
     performAction,
     func,
-    distance
+    distance,
+    newElement,
+    addCloseButton,
+    removeList,
+    checkItem
 }
