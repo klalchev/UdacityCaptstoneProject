e2c285b59abe1f7d7c325f2d9cd14b82c388e32b
Kostadin <kostadin.lalchev88@gmail.com>
2020-10-10 04:41:31 +0300

    fix: Replace URL with an object argument to fix issue with the restCoutnries Jest Test

--- _test_/restCountries.spec.js
+++ _test_/restCountries.spec.js
@@ -1,7 +1,8 @@
 import { restCountries } from "../src/client/js/app"
 
 test('testing sidet data', () => {
-    expect(restCountries('https://restcountries.eu/rest/v2/name/Bulgaria')).resolves.toBe([{
+    let countryData = {country: 'Bulgaria'};
+    expect(restCountries(countryData)).resolves.toBe([{
         'name':'Bulgaria',
         'alpha2Code':'BG',
         'capital': 'Sofia'
@@ -9,3 +10,5 @@ test('testing sidet data', () => {
         // "subjectivity_confidence":0.5427156021389409
     }])
 })
+
+//Your test gives the full restCountries URL to the restCountries function, while it only expects an object with a 'country' key. Once you match up the arguments, it seems to pass.
